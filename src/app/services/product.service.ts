6cd4c3956ec6e2e96a171ba5126c35805f22a1ba
Huy Minh <minh.th12558@sinhvien.hoasen.edu.vn>
2023-12-15 15:45:02 +0700

    update buoi04

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,8 +10,8 @@ export class ProductService {
   products = [
     { id: 1, name: 'product 1', price: 100, quantity: 1 },
     { id: 2, name: 'product 2', price: 300, quantity: 3 },
-    { id: 3, name: 'product 3', price: 300, quantity: 3 },
-    { id: 4, name: 'product 4', price: 300, quantity: 3 },
+    { id: 3, name: 'product 3', price: 200, quantity: 5 },
+    { id: 4, name: 'product 4', price: 600, quantity: 2 },
   ]
 
   getProducts() {
@@ -21,6 +21,8 @@ export class ProductService {
   handleBuyOnce(product: any) {
     console.log(product);
     product.quantity--;
+    if (product.quantity < 0)
+      product.quantity = 0;
   }
 
   deleteProduct(id: number) {
